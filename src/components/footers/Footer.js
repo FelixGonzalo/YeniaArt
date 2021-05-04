37747f91f1a292465449ae7bb738912eadfd66e2
Felix Castro <felixgonzalocastro@gmail.com>
2021-05-04 13:31:59 -0500

    mejora de footer y flujo de pedido

--- src/components/footers/Footer.js
+++ src/components/footers/Footer.js
@@ -1,5 +1,6 @@
 import './footer.css'
 import {Link} from "react-router-dom"
+import iWhatsapp from '../../img/icon/icon_whatsapp.svg'
 import iFacebook from '../../img/icon/icon_facebook.svg'
 
 const Footer = () => {
@@ -8,8 +9,8 @@ const Footer = () => {
       <div  className="footer-top">
         <div>
           <h2>YeniaArt</h2>
-          <p>Celular: 123456789</p>
-          <p>Atención virtual: 8am - 9pm</p>
+          <p>Celular : 123456789</p>
+          <p>Horario : 8am - 9pm</p>
         </div>
         <ul className="footer-lista">
           <li className="footer-lista-titulo">
@@ -34,8 +35,15 @@ const Footer = () => {
       </div>
       
       <div className="footer-bottom">
-        <p>© Todos los derechos reservados 2021</p>
-        <a href="https://www.facebook.com/YeniaArt/" target="_blank" rel="noreferrer"><img src={iFacebook} alt=""/></a>
+        <p><span>©</span> Todos los derechos reservados 2021</p>
+        <div>
+          <a href="https://api.whatsapp.com/send?phone=51957498221" target="_blank" rel="noreferrer">
+            <img src={iWhatsapp} alt=""/>
+          </a>
+          <a href="https://www.facebook.com/YeniaArt/" target="_blank" rel="noreferrer">
+            <img src={iFacebook} alt=""/>
+          </a>
+        </div>
       </div>
     </footer>
   );
--- src/components/sections/FlujoPedido.js
+++ src/components/sections/FlujoPedido.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 import './flujopedido.css'
 import ichatusuarios from '../../img/icon/chatusuarios.svg'
 import icatalogo from '../../img/icon/catalogo.svg'
 import ientrega from '../../img/icon/paquete.svg'
+import iWhatsapp from '../../img/icon/icon_whatsapp.svg'
+import iFacebook from '../../img/icon/icon_facebook.svg'
 
 const FlujoPedido = () => {
   return (
@@ -11,15 +14,27 @@ const FlujoPedido = () => {
       <div className="flujopedido-items">
         <div className="flujopedido-item">
           <img src={icatalogo} alt=""/>
-          <p>1. Ver productos en el catálogo</p>
+          <p>1. Revisa productos en el catálogo</p>
+          <Link to="/catalogo" className="flujopedido-boton"> 
+            Ver catálogo
+          </Link> 
         </div>
         <div className="flujopedido-item">
           <img src={ichatusuarios} alt=""/>
           <p>2. Contáctanos para hacer tu pedido</p>
+          <div className="flujopedido-contactanos">
+            <a href="https://api.whatsapp.com/send?phone=51957498221" target="_blank" rel="noreferrer">
+              <img src={iWhatsapp} alt=""/>
+            </a>
+            <a href="https://www.facebook.com/YeniaArt/" target="_blank" rel="noreferrer">
+              <img src={iFacebook} alt=""/>
+            </a>
+          </div>
         </div>
         <div className="flujopedido-item">
           <img src={ientrega} alt=""/>
-          <p>3. Espera la elaboración y entrega</p>
+          <p>3. Recibe tu pedido</p>
+          <p className="flujopedido-entrega">Entrega por Delivery o tiendas amigas</p>
         </div>
       </div>
     </section>
